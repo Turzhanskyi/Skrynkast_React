877634c04d179bb2eee59fc72c88bb6d4241d095
Turzhanskyi <turzhansky81@gmail.com>
2019-03-25 21:52:55 +0200

    09 Жизненный цикл React-компонентов

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,27 +1,46 @@
 import React, {Component} from 'react'
 
 class Article extends Component {
-    state = {
-       isOpen: true
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: props.defaultOpen
+        }
+    }
+
+    componentWillMount() {
+        console.log('---', 'mounting')
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('---', 'will receive props')
+        if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+            isOpen: nextProps.defaultOpen
+        })
+    }
+
+    componentWillUpdate() {
+        console.log('---', 'will update')
     }
 
     render() {
         const {article} = this.props
+        const style = {width: '50%'}
         const body = this.state.isOpen && <section className="card-text">{article.text}</section>
-        const styly = {width: '50%'}
         return (
-            <div className="card mx-auto" style={styly}>
+            <div className="card mx-auto" style = {style}>
                 <div className="card-header">
                     <h2>
                         {article.title}
                         <button onClick={this.handleClick} className="btn btn-primary btn-lg float-right">
-                            {this.state.isOpen ? 'Open' : 'Close'}
+                            {this.state.isOpen ? 'close' : 'open'}
                         </button>
                     </h2>
                 </div>
                 <div className="card-body">
                     <h6 className="card-subtitle text-muted">
-                        create date: {(new Date(article.date)).toDateString()}
+                        creation date: {(new Date(article.date)).toDateString()}
                     </h6>
                     {body}
                 </div>
@@ -30,11 +49,12 @@ class Article extends Component {
     }
 
     handleClick = () => {
-        console.log('-----', 'clicked')
+        console.log('---', 'clicked')
         this.setState({
             isOpen: !this.state.isOpen
         })
     }
 }
+
 
 export default Article
